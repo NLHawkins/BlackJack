45e9740ebbbff6d84694c087affe75a1a8eb595c
NLHawkins <nathanlhawkins@gmail.com>
2017-01-19 21:10:35 -0500

    deal card started

--- BlackJack/GamePlay.cs
+++ BlackJack/GamePlay.cs
@@ -9,7 +9,22 @@ namespace BlackJack
 {
     class GamePlay
     {
-        int rng = new Random() 
+        public static List<Card> DealCard(int count)
+        {
+
+            var gameDeck = new List<Card>(CardFactory.CreateDeck());
+            var gameCard = new Card();
+            var rng = new Random();
+
+            CardFactory.CreateDeck();
+            for (int i = 0; i < count; i++)
+            {
+                gameCard = gameDeck[rng.Next(52)];
+            }
+
+            return gameCard;
+        }
+//public Card(Suits _suit, Faces _face)
 
     }
 }
